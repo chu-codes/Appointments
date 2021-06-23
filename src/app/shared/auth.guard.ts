1a771a4e478aa818bce582da2659ee8492e5a34c
Sonam Chuki <chuclicks@gmail.com>
2021-06-23 18:51:46 +0600

    Route Guard

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
+import { NotificationService } from './notification.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private notificationService: NotificationService
+  ) {}
 
   canActivate(): boolean {
     if (this.authService.loggedIn()) {
       return true;
     } else {
       this.router.navigate(['/login']);
+      this.notificationService.warn('Please login to access Appointments');
       return false;
     }
   }
--- src/app/shared/role.guard.ts
+++ src/app/shared/role.guard.ts
@@ -7,19 +7,33 @@ import {
   Router,
 } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+import { NotificationService } from './notification.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RoleGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private notificationService: NotificationService,
+    private authService: AuthService
+  ) {}
   canActivate() {
     let Role = localStorage.getItem('role');
-    if (Role == 'admin') {
-      return true;
+    if (this.authService.loggedIn()) {
+      if (Role == 'admin') {
+        return true;
+      } else {
+        // alert('You dont have admin access');
+        this.router.navigate(['/home']);
+        this.notificationService.warn('You dont have admin access');
+        return false;
+      }
+    } else {
+      this.router.navigate(['/login']);
+      this.notificationService.warn('Please login to access Admin Dashboard');
+      return false;
     }
-    alert('You dont have admin access');
-    this.router.navigate(['/home']);
-    return false;
   }
 }
